a7b55e23ec81072735bdba973b44c53882155293
premsai-noolu <premsainoolu01@gmail.com>
2024-01-10 17:47:54 +0530

    sign In/SignUp form && Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-      Namste Everyone lets build netflix gpt
+    <div>
+      <Body />
     </div>
   );
 }
